9fbeba5738ec8bcb38faff022a7eb59e6d75f7cf
Merovingio <pistoredavide@gmail.com>
2020-04-11 15:54:26 +0200

    Test commit

--- TheBatCoreWebScrapper.Test/UnitTest1.cs
+++ TheBatCoreWebScrapper.Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Linq;
 using NUnit.Framework;
 using TheBatCoreWebScrapper.DAL;
 using TheBatCoreWebScrapper.DAL.Models;
@@ -19,6 +20,7 @@ namespace TheBatCoreWebScrapper.Test
             using (var context = new ScrapperContext())
             {
                 context.UrlLibraries.Add(new UrlLibrary() {Url = "http://www.cephei.com"});
+                context.ScrappingConfigurations.Add(new ScrappingConfiguration(){Interval = 1, UrlLibrary = context.UrlLibraries.First()});
                 //context.ScrappingResults.Add(new ScrappingResult{BodyResult = "", UrlLibrary = new UrlLibrary{Url = "http://www.google.com"}});
                 context.SaveChanges();
             }
